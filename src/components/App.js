0194101da795f081a6a010ecb7a779ccaac484b7
Sai Narendra <tannirusainarendra81@gmail.com>
2025-09-30 19:54:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,26 @@
 import React from "react";
 import './../styles/App.css';
 
+const PageLayout = ({ header, footer, children }) => {
+  return (
+    <div>
+      <header>{header}</header>
+      <main>{children}</main>
+      <footer>{footer}</footer>
+    </div>
+  );
+};
+
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <PageLayout
+            header={<h1>Welcome to my website</h1>}
+            footer={<p>© 2023 My Website. All rights reserved.</p>}
+          >
+            <p>This is the content of my website.</p>
+          </PageLayout>
     </div>
   )
 }
