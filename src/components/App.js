edcd261470386db26d9c7c1b23f5d03aad4266e7
Sai Narendra <tannirusainarendra81@gmail.com>
2025-10-15 12:55:44 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,17 @@ import React, { useState } from "react";
 import "./styles/App.css"
 
 const PageLayout = ({ header, footer, children }) => {
-  return <div>
-    <header>{header}</header>
-    <main>{children}</main>
-    <footer>{footer}</footer>
-  </div>
-}
+  return (
+    <div>
+      <header>{header}</header>
+      <main>{children}</main>
+      <footer>{footer}</footer>
+    </div>
+  );
+};
+
+export default PageLayout;
+
 
 const App = () => {
 
